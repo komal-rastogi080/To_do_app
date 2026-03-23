6e2b31ac8ccdbbecd84fd100acfa83c685910239
Komal <rastogikomal57@gmail.com>
2026-03-23 11:27:20 +0530

    first commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express()
+const todoRoutes = require('./routes/todoRoutes.js');
+
+app.use(express.json());
+app.use('/api/todos', todoRoutes);
 
 module.exports = app;
